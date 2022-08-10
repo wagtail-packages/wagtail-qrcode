101660c84506ed67ff30164f3ab855de1b55f31c
Nick Moreton <nickmoreton@me.com>
2022-08-10 21:28:58 +0100

    rename field panels

--- wagtail_qrcode/test/tests/test_field_panels.py
+++ wagtail_qrcode/test/tests/test_field_panels.py
@@ -1,14 +1,14 @@
 from django import forms
 from django.test import TestCase
 
-from wagtail_qrcode.field_panels import QrCodeSVGFieldPanel, QrCodeSVGUsageFieldPanel
+from wagtail_qrcode.field_panels import QrCodeFieldPanel, QrCodeUsageFieldPanel
 
 
 class TestFieldPanels(TestCase):
     def test_qr_code_svg_field_panel(self):
-        fp = QrCodeSVGFieldPanel("foo", widget=forms.TextInput, classname="full")
+        fp = QrCodeFieldPanel("foo", widget=forms.TextInput, classname="full")
         fp.field_name = "foo"
 
     def test_qr_code_svg_usage_field_panel(self):
-        fp = QrCodeSVGUsageFieldPanel("bar", widget=forms.TextInput, classname="full")
+        fp = QrCodeUsageFieldPanel("bar", widget=forms.TextInput, classname="full")
         fp.field_name = "bar"
