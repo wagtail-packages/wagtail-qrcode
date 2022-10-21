25fe217b79b9462b3793ad7d08b69eb1a4f5906e
Nick Moreton <nickmoreton@me.com>
2022-10-21 22:52:53 +0100

    Add version compatibility

--- wagtail_qrcode/test/models.py
+++ wagtail_qrcode/test/models.py
@@ -1,14 +1,20 @@
 from wagtail import VERSION as WAGTAIL_VERSION
-from wagtail.admin.panels import (
-    FieldPanel,
-    MultiFieldPanel,
-    ObjectList,
-    TabbedInterface,
-)
 
 if WAGTAIL_VERSION >= (3, 0):
+    from wagtail.admin.panels import (
+        FieldPanel,
+        MultiFieldPanel,
+        ObjectList,
+        TabbedInterface,
+    )
     from wagtail.models import Page
 else:
+    from wagtail.admin.edit_handlers import (
+        FieldPanel,
+        MultiFieldPanel,
+        ObjectList,
+        TabbedInterface,
+    )
     from wagtail.core.models import Page
 
 from wagtail_qrcode.admin_forms import QrCodeEmailForm
