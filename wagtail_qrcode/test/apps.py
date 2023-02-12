829905c0d31a58843a9c694d5bc99557a39d5351
Nick Moreton <nickmoreton@me.com>
2023-02-12 22:20:49 +0000

    Delete files

--- wagtail_qrcode/test/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class WagtailQrcodeTestAppConfig(AppConfig):
-    label = "wagtail_qrcode_test"
-    name = "wagtail_qrcode.test"
-    verbose_name = "Wagtail qrcode tests"
